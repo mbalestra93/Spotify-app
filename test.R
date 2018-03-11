8439d8deb1913b3b5de07a1347ab078396875ae3
Marco <m.fogli@live.com>
2018-03-11 13:39:13 +0100

    updated my code

--- test.R
+++ test.R
@@ -25,3 +25,56 @@ maybe <- unique(final$RegionCode)
 
 test <- final[, list(n_likes = sum(internationality)), by = maybe]
 
+
+
+
+
+#HOFSTEDE
+hofstede$COUNTRY <- as.character(hofstede$COUNTRY)
+
+#Function for individual measure plot
+get_plot <- function(var){
+  return( 
+    ggplot(data=hofstede, aes(x=`ISO CODE`, y=var)) + geom_bar(stat = "identity")
+    )
+}
+
+get_plot(hofstede$Individualism)
+get_plot(hofstede$Masculinity)
+...
+
+
+ggplot(data=hofstede, aes(x=colnames(hofstede.trans)[3:8], y=hofstede$COUNTRY[1], fill=hofstede$COUNTRY)) + geom_bar(stat = "identity")
+
+#function for individual country plot
+
+#create transpoded data frame
+hofstede.trans <- transpose(hofstede)
+colnames(hofstede.trans) = hofstede.trans[2, ] 
+hofstede.trans <- hofstede.trans[-c(1, 2),]
+rownames(hofstede.trans) = c("Power Distance", "Individualism", "Masculinity", "Uncertainty Avoidance", "Long Term Orientation", "Indulgence")
+
+#new function
+get_country_plot <- function(i){
+  return( 
+    ggplot(data=hofstede.trans, aes(x=rownames(hofstede.trans), y=hofstede.trans[, i])) + geom_bar(stat = "identity")
+  )
+}
+
+#uses the function
+get_country_plot("Canada")
+get_country_plot("France")
+
+
+#comparing plots
+
+compared_plot <- function(i, j){
+  dt.temp <- subset(df, COUNTRY==i | COUNTRY==j)
+  return(
+    ggplot(data=dt.temp, aes(x=var_type, y=dt.temp$value, fill=COUNTRY)) + geom_bar(stat = "identity", position = position_dodge())
+  )
+}
+
+#using function
+compared_plot("Canada", "France")
+
